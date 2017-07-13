66bd8caf3155b0d9b21afdc37d210f02b81f034f
jp <gaines.johnp@gmail.com>
2017-07-13 10:39:58 -0700

    changed model to reflect new direction

---
+++ css/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var locations = [];
+
+function Location() {
+  this.date = date;
+  this.category = category;
+  this.city = city;
+  this.inspiration = inspiration;
+  this.body = body;
+  this.author = author;
+}
