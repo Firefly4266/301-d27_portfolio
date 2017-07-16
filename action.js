a47e9700d33ab6b1352761af61effdbe83c239bc
jp <gaines.johnp@gmail.com>
2017-07-15 20:19:10 -0700

    filter populating

--- action.js
+++ action.js
@@ -5,7 +5,11 @@ var articleView = {};
 articleView.populateFilters = function() {
   $('article').not('.template').each(function() {
     var template = $('template').html();
-    var val = $(this).find();
+    var val = $(this).attr('data-city');
+    console.log($(this).attr('data-city'));
+    
+    var optionTag = '<option value="' + val + '">' + val + '</option>';
+    $('#city-filter').append(optionTag);
   });
 };
 
