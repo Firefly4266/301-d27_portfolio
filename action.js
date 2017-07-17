ebd0f18beae3483825a9af19b1c499f986a4ad0a
jp <gaines.johnp@gmail.com>
2017-07-17 02:54:21 -0700

    updating branch

--- action.js
+++ action.js
@@ -3,5 +3,30 @@
 var articleView = {};
 
 articleView.populateFilters = function() {
-  $('article').each(function() {
-    var optionTag, city;
+  $('article').not('.template').each(function() {
+    var template = $('.template').html();    
+    var val = $(this).attr('data-city');
+    var optionTag = '<option value="' + val + '">' + val + '</option>';
+    /*This conditional checks to see if the current value is the same as the value we are on.
+    if so it will not append it so no dupe in the option tag*/
+    if($('#city-filter option[value="' + val + '"]').length === 0) {
+      $('#city-filter').append(optionTag);
+    }
+  });
+};
+
+articleView.handleCityFilter = function() {
+  $('article.template').hide();
+  $('#city-filter').on('change', function(){    
+    if($(this).val()){
+      $('article').hide();    
+      $('article[data-city="' + $(this).val() + '"]').show();
+    }else {
+      $('article').show();
+      $('article.template').hide();
+    }
+  });
+};
+
+articleView.handleCityFilter();
+articleView.populateFilters();
