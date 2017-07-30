b435242521736c6a9a587d724a8b62e8678a1c4c
Firefly4266 <johnathon@Icon-Base.(none)>
2017-07-30 02:49:43 -0700

    adding page functionality

--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const pg = require('pg');
+const port = process.env || 5000;
 
-let app = express();
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+let conString = process.env.PG_PASSWORD;
+const client = new pg.client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('./public'));
 
 app.listen(5000, ()=> {
   console.log('Server up on Port: 5000');
