13b2c035e1c48df937189c08749c538d3ffe90e5
jp <gaines.johnp@gmail.com>
2017-07-29 19:34:06 -0700

    changed connection string

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-let conString = process.env.PG_PASSWORD;
+let conString = process.env.PG_PASSWORD || process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
