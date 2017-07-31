d81d61cfe54797415b16525f98a02011a50f415e
Firefly4266 <johnathon@Icon-Base.(none)>
2017-07-31 01:21:15 -0700

    con string issues

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const conString = process.env.DATABASE_URL || process.env.PG_PASSWORD;
+const conString = process.env.PG_PASSWORD || process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
